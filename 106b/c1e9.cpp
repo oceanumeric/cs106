a53cfc58ada3890b278eab978c1733936c5ce6c8
oceanumeric <numerical.ocean@gmail.com>
2021-11-05 00:20:28 +0100

    [add] prime factorization

---
+++ 106b/c1e9.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+bool prime(int n) {
+    bool is_prime = true; 
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            is_prime = false; 
+        }
+    }
+    return is_prime;
+}
+
+int main () {
+
+    int n = 0; 
+    int factor = 1; 
+    std::cout << "This program factors a number." << std::endl; 
+    std::cout << "Enter a positive integer to be factored: ";
+    std::cin >> n; 
+    // find all prime numbers upto n
+    for (int i = 2; i < n; i++) {
+        if (prime(i)) {
+            while (n % i == 0) {
+                std::cout << i << std::endl;
+                n /= i; 
+                if (n == 1) break; 
+            }
+        }
+    }
+    std::cout << n << std::endl;
+    return 0; 
+}
