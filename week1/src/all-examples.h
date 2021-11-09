155120a5b42a01a39088627fdff3a060c8b587c6
oceanumeric <numerical.ocean@gmail.com>
2021-11-09 12:22:32 +0100

    [build] environment

---
+++ week1/src/all-examples.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// functions we use in all-examples.cpp
+
+int factorial(int number); // definition before use
+void printTenTimes(char c);
+int doubleValue(int x);
+void doubleValueWithRef(int &x);
+int square(int x);
+bool even(int value);
