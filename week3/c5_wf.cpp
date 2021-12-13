5a8df6b7579f36d78ae4411f545b63eea24fd293
oceanumeric <numerical.ocean@gmail.com>
2021-12-13 18:24:00 +0100

    [update] c5_wf.cpp

--- week3/c5_wf.cpp
+++ week3/c5_wf.cpp
@@ -4,41 +4,44 @@
 #include <vector>
 #include <cctype>
 #include <map>
+#include <algorithm>
 
-// convert a word to lower case
+
 std::vector <std::string> line_to_word(std::string line); 
 std::string tokenize(std::string word); 
+void print_top_ten(std::map <std::string, int> words_frequency, int frequency); 
 
 
 // read a text file and calculate words frequency 
 
 int main() {
+    // create an empty map (dict)
     std::map <std::string, int> words_frequency; 
+    // read the text file 
     std::ifstream myfile("shakespeare.txt");   
     std::string line;
     int line_num = 0; 
     if (myfile.is_open()) {
+        // stream each line 
         while (std::getline(myfile, line)) {
-            if (line_num <= 20) {
-                std::vector <std::string> words = line_to_word(line);
-
-                for (auto & elem: words) {
-                    std::string temp = tokenize(elem); 
-                    if (!words_frequency.count(temp)) {
-                        words_frequency.insert(std::pair<std::string, int>(temp, 1));
-                    } else {
-                        words_frequency[temp] ++; 
-                    }
-
+            // convert a string into an vector of words 
+            std::vector <std::string> words = line_to_word(line);
+            // tokenize words and calculate  the frequency 
+            for (auto & elem: words) {
+                std::string temp = tokenize(elem); 
+                if (!words_frequency.count(temp)) {
+                    words_frequency.insert(std::pair<std::string, int>(temp, 1));
+                } else {
+                    words_frequency[temp] ++; 
                 }
-                
             }
-            line_num += 1;
+                
         }
         myfile.close();
-        for (auto i: words_frequency) {
-                    std::cout << '{' << i.first << ": " << i.second << '}' <<std::endl; 
-                } 
+        
+        // print the top 10 freqency words 
+        print_top_ten(words_frequency, 10); 
+
     } else {
         std::cout << "Unable to open the file" << std::endl; 
     }
@@ -91,3 +94,25 @@ std::string tokenize(std::string word) {
 
 // sort map based on values
 // https://www.educative.io/edpresso/how-to-sort-a-map-by-value-in-cpp
+
+bool sort_by_value(std::pair<std::string, int> &a,
+                   std::pair<std::string, int> &b){
+                       
+                       return (a.second > b.second); 
+}
+
+
+void print_top_ten(std::map <std::string, int> words_frequency, int frequency) {
+    std::vector<std::pair<std::string, int>> words_vec;  // create an empty vector
+    // copy key-value pairs from the map to the vector
+    for (auto elem : words_frequency) {
+        words_vec.push_back(std::make_pair(elem.first, elem.second)); 
+    }
+
+    // sort the vector
+    std::sort(words_vec.begin(), words_vec.end(), sort_by_value);
+
+    for (int i=0; i<frequency; i++) {
+        std::cout << words_vec[i].first << " : " << words_vec[i].second << std::endl; 
+    }
+}
